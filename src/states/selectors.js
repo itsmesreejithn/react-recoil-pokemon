8a90d1beaa51092a659e042fb120527fd3bd7a46
Sreejith N <jithupreethy@gmail.com>
2023-12-05 18:00:27 +0530

    filtering implemented

--- src/states/selectors.js
+++ src/states/selectors.js
@@ -1,10 +1,23 @@
 import { selector } from "recoil";
-import { pokemonListAtom } from "./atoms";
+import { pokemonListAtom, pokemonListFiltersAtom } from "./atoms";
 
 export const pokemonListFilteredSelector = selector({
     key: "pokemonListFilteredSelector",
     get: ({get}) => {
         const pokemonList = get(pokemonListAtom);
-        return pokemonList;
+        const pokemonFilter = get(pokemonListFiltersAtom);
+        switch(pokemonFilter) {
+            case "show_fire":
+                return pokemonList.filter(({element}) => element === "fire");
+            
+            case "show_water":
+                return pokemonList.filter(({element}) => element === "water");
+
+            case "show_plant":
+                return pokemonList.filter(({element}) => element === "plant");
+
+            default:
+                return pokemonList;
+        }
     }
 });
