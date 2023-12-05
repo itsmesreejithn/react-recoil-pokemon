5b1b53e087308e37e5551cdc0dd1e4a63bc94aa7
Sreejith N <jithupreethy@gmail.com>
2023-12-05 20:42:17 +0530

    bug fixed in filtering operation

--- src/components/PokemontListFilter.jsx
+++ src/components/PokemontListFilter.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useRecoilState } from "recoil";
 import { pokemonListFiltersAtom } from "../states/atoms";
 
@@ -9,10 +8,10 @@ const PokemontListFilter = () => {
   };
   return (
     <div>
-      <button onClick={onFilterClick(filter)}>show all</button>
-      <button onClick={onFilterClick("show_fire")}>show fire</button>
-      <button onClick={onFilterClick("show_water")}>show water</button>
-      <button onClick={onFilterClick("show_plant")}>show plant</button>
+      <button onClick={() => onFilterClick(filter)}>show all</button>
+      <button onClick={() => onFilterClick("show_fire")}>show fire</button>
+      <button onClick={() => onFilterClick("show_water")}>show water</button>
+      <button onClick={() => onFilterClick("show_plant")}>show plant</button>
     </div>
   );
 };
