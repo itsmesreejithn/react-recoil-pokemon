356161a8eb8ce6145bc77710e94eb6e7c71a13d2
Sreejith N <jithupreethy@gmail.com>
2023-12-05 17:55:12 +0530

    merging main branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import PokemonList from "./components/PokemonList";
+import PokemontListFilter from "./components/PokemontListFilter";
 
 const App = () => {
   return (
     <div>
+      <PokemontListFilter />
       <PokemonList />
     </div>
   );
